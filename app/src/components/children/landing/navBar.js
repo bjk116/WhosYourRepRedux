cfddb0638ffd98d097974f4dc8fc03776a02e123
Winston Lum <meow@Winstons-MacBook-Pro.local>
2017-09-03 16:55:23 -0400

    added navbar and started trending

--- app/src/components/children/landing/navBar.js
+++ app/src/components/children/landing/navBar.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const NavComponent = React.createClass({
+const NavBar = React.createClass({
   render: function() {
     return (
-      <nav>
+      <nav class="navbar navbar-default">
+			  <div class="container-fluid">
+			    <div class="navbar-header">
+			      <a class="navbar-brand" href="#">Who's Your Rep?</a>
+			    </div>
 
-      </nav>
+			    <ul class="nav navbar-nav">
+			      <li class="active"><a href="#">Home</a></li>
+			      <li><a href="#">Page 1</a></li>
+			      <li><a href="#">Login</a></li>
+			    </ul>
+			  </div>
+			</nav>
     );
   }
 });
 
-ReactDOM.render(<NavComponent />, document.querySelector("navbar"));
+ReactDOM.render(<NavBar />, document.querySelector("navbar"));
---
+++ app/src/components/children/landing/trending.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Trending = React.createClass({
+  render: function() {
+    return (
+   
+    );
+  }
+});
+
+ReactDOM.render(<Trending />, document.querySelector("trending"));
