c9cd071afb725633ad68e6e1a494f467a2867d8c
unknown <jaylesh27@gmail.com>
2017-09-07 16:30:19 -0400

    added render method, fixed merge issue in app.js

--- app/src/components/children/state/subcomponents/DonorChart.js
+++ app/src/components/children/state/subcomponents/DonorChart.js
@@ -5,6 +5,12 @@ class DonorChart extends React.Component {
 	constructor(props) {
 		super(props);
 	}
+
+	render() {
+		return(
+			<h1>Industry donors chart</h1>
+		);
+	}
 }
 
 export default DonorChart;
