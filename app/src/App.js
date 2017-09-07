476b7c7bc1c819b8d0cf9cb2e99f754f3460c86b
Winston Lum <meow@Winstons-MacBook-Pro.local>
2017-09-07 07:41:13 -0400

    stuff was done

--- app/src/App.js
+++ app/src/App.js
@@ -5,9 +5,9 @@ import Header from './components/children/Header';
 import Footer from './components/children/Footer';
 import Main from './components/Main';
 import Calendar from './components/children/calendar/Calendar';
-import PieChart from './components/children/chart/Chart';
-import NavBar from './components/children/landing/navBar';
-import SearchBar from './components/children/landing/searchBar';
+import PieChart from './components/children/chart/chart';
+import NavBar from './components/children/landing/NavBar';
+import SearchBar from './components/children/landing/SearchBar';
 
 class App extends Component {
   render() {
