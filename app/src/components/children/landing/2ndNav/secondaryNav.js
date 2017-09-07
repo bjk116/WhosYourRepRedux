7f72a342023b7ebb70ab9a2df47230c784602dd8
Winston Lum <meow@Winstons-MacBook-Pro.local>
2017-09-07 18:09:38 -0400

    moved second nav bar

--- app/src/components/children/landing/2ndNav/secondaryNav.js
+++ app/src/components/children/landing/2ndNav/secondaryNav.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 export class NavBar extends React.Compnent {
+
+  state = {signedIn: false};
+
+  handleChange = (event, logged) => {
+    this.setState({logged: logged});
+  };
+
   render() {
     const pages = ["Home", "" , ""];
     const links = pages.map(page => {
@@ -10,7 +17,8 @@ export class NavBar extends React.Compnent {
         </a>
       )
     });
-    return <nav>{navLinks}</nav>;
+    return <nav>{links}</nav>;
+    <Login />
   }
 }
 
