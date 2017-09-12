8414b02023b73d0077ef87ac334f97d2daa741d0
Rebecca Renavitz <rlrenavitz@gmail.com>
2017-09-11 21:19:17 -0400

    Fixed merge error

--- app/src/components/Navbar/UpdatedNavBar.js
+++ app/src/components/Navbar/UpdatedNavBar.js
@@ -8,11 +8,11 @@ class UpdatedNavBar extends React.Component {
 			<nav>
 				<div className="nav-wrapper">
 					<ul id = "navbar-left" className="left">
-<<<<<<< HEAD
+
 						<li className="active"><Link to="logo.png"><a href="logo.png">Portfolio</a></Link></li>
-=======
+
 						<li className="active"><a href="/test">Logo Image</a></li>
->>>>>>> 9b343110a73829c108e58a399d0379c81d08dae4
+
 						<li className="active"><Link to="/">RepGenius</Link></li>
 						<li id="nav-title">Who's Your Rep?</li>
 					</ul>
