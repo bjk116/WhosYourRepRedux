ef196a24d24a3a8b5efb2cf2cf593ce4dbb520cd
jaylesh27 <jaylesh27@gmail.com>
2017-09-13 18:30:06 -0400

    attempts to fix broke facebook login

--- src/components/Navbar/UpdatedNavBar.js
+++ src/components/Navbar/UpdatedNavBar.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import "./style.css";
+import axios from 'axios';
 
 class UpdatedNavBar extends React.Component {
+
+	handleFacebookClick(e) {
+		e.preventDefault();
+		console.log("clicked on facebook login");
+		axios({
+			url: "/auth/facebook",
+			method: "GET",
+		}).then((resp)=>{
+			console.log(resp);
+		});
+	}
+
 	constructor(props){
 		super(props);
 		//going to use props as the second part of the ahref link, ie 
 		//<a href={"/state/" + this.prop.stateParam}>State</a>
+		this.handleFacebookClick = this.handleFacebookClick.bind(this);
 	}
 	
 	render() {
@@ -17,13 +31,13 @@ class UpdatedNavBar extends React.Component {
 					<ul id="nav-mobile" className="hide-on-small-only" className="left" >
 						<li><img src="logo2.png" id="logo-img" className="hide-on-small-only"/></li>
 						<li className="active" className ="hide-on-small-only">RepGenius</li>
-						<li id="nav-title" class="hide-on-small-only">Who's Your Rep?</li>
+						<li id="nav-title" className="hide-on-small-only">Who's Your Rep?</li>
 					</ul>
 
 					<ul id="nav-mobile" className="hide-on-small-only" className="right">
 						<li className ="active" className ="hide-on-small-only">Calendar</li>
 						<li className ="active" className ="hide-on-small-only">State</li>
-						<li className="active" className ="hide-on-small-only"><a href="/auth/facebook">Login with Facebook</a></li>
+						<li className="active" className ="hide-on-small-only"><a href="#" onClick={this.handleFacebookClick}>Login with Facebook</a></li>
 					{/*<li className="active"><a href="http://localhost:3000/logout">Logout</a></li>*/}
 					</ul>
 				</div>
